68028de2425425088e6289204d9f24649bf45f6e
arseil <84498832+arseil@users.noreply.github.com>
2022-10-23 15:52:15 +0300

    some fixes

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,3 +4,20 @@ $(document).ready(function() {
         menu: '#myMenu'
     });
 });
+
+$(document).ready(function() {
+    document.querySelectorAll('.accordeon__heading').forEach(function(item) {
+        item.addEventListener('click', function() {
+            if (!item.classList.contains('ins')) {
+                document.querySelectorAll('.accordeon__heading').forEach(function(newItem) {
+                    if (newItem.classList.contains('ins')) {
+                        $(newItem).toggleClass('ins').next().slideToggle();
+                    }
+                });
+                $(item).toggleClass('ins').next().slideToggle();
+            } else {
+                $(item).toggleClass('ins').next().slideToggle();
+            }
+        });
+    });
+});
