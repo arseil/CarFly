97b6e4376a92a8db27494677eee91c0fff6c0fa9
arseil <84498832+arseil@users.noreply.github.com>
2022-10-25 23:58:21 +0300

    added slick slider

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
         anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
         menu: '#myMenu'
     });
+
+    $('.buy__wrapper').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+      });
 });
 
 $(document).ready(function() {
