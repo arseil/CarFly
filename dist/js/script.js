36404f30c9bdba476d887ba38bd943b7eeb99126
arseil <84498832+arseil@users.noreply.github.com>
2022-10-26 22:05:32 +0300

    replace svg element with pseudo element before, added mixins

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
 	$('#pagepiling').pagepiling({
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
-        menu: '#myMenu'
-    });
+		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
+		menu: '#myMenu'
+	});
 
-    $('.buy__wrapper').slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+	$('.buy__wrapper').slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
-      });
+	  });
 });
 
 $(document).ready(function() {
-    document.querySelectorAll('.accordeon__heading').forEach(function(item) {
-        item.addEventListener('click', function() {
-            if (!item.classList.contains('ins')) {
-                document.querySelectorAll('.accordeon__heading').forEach(function(newItem) {
-                    if (newItem.classList.contains('ins')) {
-                        $(newItem).toggleClass('ins').next().slideToggle();
-                    }
-                });
-                $(item).toggleClass('ins').next().slideToggle();
-            } else {
-                $(item).toggleClass('ins').next().slideToggle();
-            }
-        });
-    });
+	document.querySelectorAll('.accordeon__heading').forEach(function(item) {
+		item.addEventListener('click', function() {
+			if (!item.classList.contains('ins')) {
+				document.querySelectorAll('.accordeon__heading').forEach(function(newItem) {
+					if (newItem.classList.contains('ins')) {
+						$(newItem).toggleClass('ins').next().slideToggle();
+					}
+				});
+				$(item).toggleClass('ins').next().slideToggle();
+			} else {
+				$(item).toggleClass('ins').next().slideToggle();
+			}
+		});
+	});
 });
