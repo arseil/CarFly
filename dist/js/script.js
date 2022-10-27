f902a5385afa7bc83211779273553149b9f83117
arseil <84498832+arseil@users.noreply.github.com>
2022-10-27 22:24:57 +0300

    some fixes

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
 	  });
+
 });
 
 $(document).ready(function() {
@@ -30,4 +31,21 @@ $(document).ready(function() {
 			}
 		});
 	});
+});
+
+$('img.img-svg').each(function() {
+	var $img = $(this);
+	var imgClass = $img.attr('class');
+	var imgURL = $img.attr('src');
+	$.get(imgURL, function(data) {
+		var $svg = $(data).find('svg');
+		if (typeof imgClass !== 'undefined') {
+			$svg = $svg.attr('class', imgClass + ' replaced-svg');
+		}
+		$svg = $svg.removeAttr('xmlns:a');
+		if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+			$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+		}
+		$img.replaceWith($svg);
+	}, 'xml');
 });
